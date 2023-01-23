cf69d6204efbe15d7db55d0e68c5d69befba39a7
Chloe Court <crc.courtois@gmail.com>
2023-01-23 22:49:29 +0100

    Update webpack.config.js
    to deploy with vercel changed lined 7 in webpack from dist to public

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './client/main.js',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
   },
   plugins: [
